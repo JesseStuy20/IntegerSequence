a1a20b4bb6f2feb56b71c6cc3d91e8732481b966
JesseStuy20 <jxie20@stuy.edu>
2020-12-08 12:33:51 -0500

    Added ArraySequence with constructor and private variables

---
+++ ArraySequence.java
@@ -0,0 +1,10 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence {
+  private int currentIndex ;
+  private int[] data ;
+
+  public ArraySequence(int [] other){
+    currentIndex = 0 ;
+    data = other ;
+  }
+}
