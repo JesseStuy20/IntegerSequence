db0d60e5ffc82c400d68c9dc5200e4d35a642f85
JesseStuy20 <jxie20@stuy.edu>
2020-12-07 22:59:33 -0500

    Added more test cases

--- RangeTester.java
+++ RangeTester.java
@@ -5,6 +5,18 @@ public class RangeTester {
     while (s.hasNext()) {
       System.out.println(s.next()) ;
     }
-
+    try {
+      s.next() ;
+    }
+    catch (RuntimeException e) {
+      System.out.println("No more values") ;
+    }
+    IntegerSequence r = new Range(10,20);
+    while(r.hasNext()) {
+      System.out.print(r.next()) ;
+      if(r.hasNext())
+         System.out.print(", ") ;
+   }
+    System.out.println( );
   }
 }
